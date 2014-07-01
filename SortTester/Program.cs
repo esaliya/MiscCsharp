03b63a28945eee4060b17939141982b9745c05b0
Saliya Ekanayake <esaliya@gmail.com>
2014-07-01 11:28:31 -0400

    Initial commit with sort tester

---
+++ SortTester/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SortTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = 10;
+            var values = new double[num];
+            var indices = new int[num];
+            for (var i = 0; i < num; ++i)
+            {
+                values[i] = i;
+                indices[i] = num - i;
+            }
+            Console.WriteLine(string.Join(",", values));
+            Array.Sort(indices,values);
+            Console.WriteLine(string.Join(",", values));
+            Console.Read();
+        }
+    }
+}
